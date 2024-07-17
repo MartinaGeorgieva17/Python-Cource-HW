20312850fb051d801e23d2b06268c684b8116ca5
MartinaGeorgieva17 <martina.1990.1990@gmail.com>
2024-07-17 22:41:22 +0300

    mysql

--- Exercises Python/Exercises Lekciq 38 17.07.2024/basic_command.sql
+++ Exercises Python/Exercises Lekciq 38 17.07.2024/basic_command.sql
@@ -1,19 +1,23 @@
-CREATE TABLE testTable(
-    user_name VARCHAR (50),
+-- Create table `testTable`
+CREATE TABLE testTable (
+    user_name VARCHAR(50),
     user_age TINYINT UNSIGNED,
     price FLOAT UNSIGNED,
     user_comment TEXT,
-    smoker ENUM('yes', 'no'), 
-    PRIMARY KEY
+    smoker ENUM('yes', 'no'),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_name)  -- Assuming user_name is intended as primary key
 );
 
 -- Insert one row
-INSERT INTO `testTable` VALUES('Ada', 50, 34.5, '', 'yes');
+INSERT INTO testTable (user_name, user_age, price, user_comment, smoker)
+VALUES ('Ada', 50, 34.5, '', 'yes');
 
 -- Insert multiple rows with specified columns
-INSERT INTO `testTable` (user_name, price, user_age) VALUES 
-('Pesho', 34.5, 34);
+INSERT INTO testTable (user_name, price, user_age)
+VALUES ('Pesho', 34.5, 34);
 
-ALTER TABLE `testTable`
-ADD COLUMN `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+-- Alter table `testTable` to add `created_at` column
+ALTER TABLE testTable
+ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
